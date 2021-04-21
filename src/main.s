3af7d5156105e0be78159fa26f1f102de694beb4
houtanf <houtanfaridi@gmail.com>
2021-04-20 22:34:09 -0500

    IT WORKSS

--- src/main.s
+++ src/main.s
@@ -1,42 +1,64 @@
+# Adds command line arguments and returns result as the exit code.
+
+
 .intel_syntax noprefix
 .section .text
 .globl my_atoi
 
 my_atoi:
-  push rbp
-  mov rbp, rsp
+  mov r11, rdi
+find_null:      # loop till r11 points to string terminating char
+  cmp byte ptr [r11], 0
+  je parse_init
+  inc r11
+  jnz find_null
 
-  mov rax, 1
+parse_init:
+  xor rax, rax    # zero out accumilator
+  mov r10, 1      # muiltiplication factor
+  
+parse_loop:
+  dec r11
+  movzx rcx, byte ptr [r11]
 
-  mov rsp, rbp
-  pop rbp
+  sub rcx, 48     # char digit to int
+  imul rcx, r10
+  add rax, rcx
+  imul r10, 10
+
+  cmp rdi, r11
+  jne parse_loop
+
   ret
   
 
 .globl _start
 
 _start:
-  mov rdx, qword ptr [rsp]  # move argc into rdx
-  mov rbx, qword ptr [rsp + 8] # get argv base
+  mov r12, qword ptr [rsp]  # move argc into rdx
+  lea rbx, qword ptr [rsp + 8] # get argv base
 
   push rbp
   mov rbp, rsp
 
-  xor rdi, rdi    # zero out to use as accumilator
+  xor r15, r15    # zero out to use as accumilator
 loop:
-  cmp rdx, 1
+  dec r12
+  cmp r12, 0
   jle return       # info no arguments left, jump to end
 
-  push qword ptr [rbx + 8 * rdx]
+  #push qword ptr [rbx + 8 + 8 * r12]
+  mov rdi, qword ptr [rbx + 8 * r12]
+  #lea rdi, qword ptr [rbx + r12 * 8]
   call my_atoi
-  add rsp, 8
-  add rdi, rax
+  #add rsp, 8
+  add r15, rax
   
-  sub rdx, 1 
   jmp loop
 
 return:
   mov rsp, rbp
   pop rbp
+  mov rdi, r15    # set result as return code
   mov rax, 60
   syscall
